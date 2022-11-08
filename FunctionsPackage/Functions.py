d3c486e2c57687b9a59da3a9e993fb7bcbdc2458
Michael <Michael@10.0.0.108>
2022-11-08 12:06:42 -0500

    Added a function to grab API data and call it in the main

--- FunctionsPackage/Functions.py
+++ FunctionsPackage/Functions.py
@@ -1,3 +1,11 @@
 # Functions.py
 import requests
 import json
+
+# This function will format a provided API 
+def GrabAPIData(x):
+    response = requests.get(x)
+    json_string = response.content
+    
+    parsed_json = json.loads(json_string) # Now we have a python dictionary
+    return parsed_json 
--- MainPackage/Main.py
+++ MainPackage/Main.py
@@ -1,5 +1,9 @@
 # Main.py
 import requests
 import json
+from FunctionsPackage.Functions import *
 
 # https://api.nasa.gov
+
+APIData = GrabAPIData('https://api.nasa.gov/neo/rest/v1/feed?start_date=2022-11-01&end_date=2022-11-08&api_key=fBoYX5TUzDqTxIRecScifonqnrIXs1wrXqF98D1R')
+print(APIData)
